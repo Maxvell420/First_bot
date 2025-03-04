7263bde5c391322eb023e3267a623d56e53a8f10
dmitrii <dmitrii@dmitrii-tyr.IGD_Rostelecom>
2025-03-04 22:31:23 +0800

    2025-03-04 первый коммит с ноута

--- date/calendar.py
+++ date/calendar.py
@@ -1,5 +1,7 @@
 input_date = str(input("Укажите дату(дд.мм.гггг): "))
 
 class date:
-    def __init__(self):
+    def __init__(self, time: str = "0"):
+        pass
+    def date(self):
         pass
