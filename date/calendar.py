028570eadde804835b629d04d5637e1ddd6b59e0
dmitrii <dmitrii@dmitrii-tyr.IGD_Rostelecom>
2025-03-05 13:20:34 +0800

    nout_merge

--- date/calendar.py
+++ date/calendar.py
@@ -1,5 +1,13 @@
 input_date = str(input("Укажите дату(дд.мм.гггг): "))
 
 class date:
+<<<<<<< HEAD
     def __init__(self):
+=======
+    def __init__(self, time: str = "0"):
+        pass
+    def date(self):
+        pass
+    def date(self):
+>>>>>>> f7ebbf1 (2025-03-05 что не так)
         pass
