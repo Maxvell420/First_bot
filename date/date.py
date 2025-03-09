86edec4890d321cbadd5d8b79820d71e47cfd223
I-am-powerman <seredkin.dmitrii.powerman@gmail.com>
2025-03-09 19:25:56 +0800

    задача стала яснее

--- date/date.py
+++ date/date.py
@@ -1,12 +1,12 @@
-start_date = "2025-12-31"
-
 
 class Date:
-    def __init__(self, time: str = "0"):
-        self.time: int = time
-        self.dateTime: bool = self._is_date(time)
+    def __init__(self, time: str = "дата не указана"):
+        self.time: str = time
+        
 
-    
+    def days_of_the_year(self):
+        
+        return f"days_of_the_year"
 
 
 
@@ -14,5 +14,6 @@ class Date:
 # По ходу решения задачи ты будешь делать ветку с помощью команды git checkout -b изменять этот класс
 # И я буду ревьювить код
 
-date = Date()
-print(date)
+date = Date("2025-03-09")
+print(date.time)
+print(date.days_of_the_year())
