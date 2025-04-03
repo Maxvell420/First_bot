c4a462e938a3818c87ee201dc08651a6bb084820
Maxvel <velichko.m@hub.sknt.ru>
2025-04-03 18:55:30 +0300

    продолжение ебли..

---
+++ tests/unit/test_time_interval.py
@@ -0,0 +1,14 @@
+import pytest
+
+help("app")
+# from app import *
+
+# from .app.date import Date
+
+
+def test_exact_100_days():
+    # date_1 = Date("2000-01-01")
+    # date_2 = Date("2000-01-02")
+    # interval = Interval(date_1, date_2)
+    # assert interval.interval() == 1
+    pass
