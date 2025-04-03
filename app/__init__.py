b64f63292fc839a82841c05dce9ed09e69f09639
Maxvel <velichko.m@hub.sknt.ru>
2025-04-03 18:55:40 +0300

    продолжение ебли..

---
+++ app/__init__.py
@@ -0,0 +1,2 @@
+from .time_interval import date as Date
+from .time_interval import interval as Interval
--- app/time_interval/date.py
+++ app/time_interval/date.py
@@ -8,15 +8,15 @@ class Date:
 
     def _parse_date(self, time: str) -> list[int]:
         try:
-            year =  int(time.split("-")[0])
-            month =  int(time.split("-")[1])
+            year = int(time.split("-")[0])
+            month = int(time.split("-")[1])
             days = int(time.split("-")[2])
         except:
             raise TypeError("Не верный формат даты!")
-        
+
         return [year, month, days]
-    
-    def _validation(self, year: int, month: int, days: int):         
+
+    def _validation(self, year: int, month: int, days: int):
         if 0 > year > 9999:
             raise ValueError("Дата не входит в допустимые границы")
         if 1 > month > 12:
@@ -24,40 +24,52 @@ class Date:
         if days > self._num_days_in_a_month(month, year) < 1:
             raise ValueError("Дата не входит в допустимые границы")
 
-    def _verification_of_high_year(self, year): # проверка на високосный год
+    def _verification_of_high_year(self, year):  # проверка на високосный год
         if year // 400 == 0:
             return True
         elif year / 4 * 10 % 10 == 0:
             return True
-        else:
-            return False 
+        return False
 
-    def _num_days_in_a_month(self, month, year): # высчитывает кол. дней в месяце и прибавляет если високосный
-        list_days_in_a_month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 
-                                 6: 30, 7: 31, 8: 31, 9: 30, 10: 31,
-                                 11: 30, 12: 31}
+    def _num_days_in_a_month(
+        self, month, year
+    ):  # высчитывает кол. дней в месяце и прибавляет если високосный
+        list_days_in_a_month = {
+            1: 31,
+            2: 28,
+            3: 31,
+            4: 30,
+            5: 31,
+            6: 30,
+            7: 31,
+            8: 31,
+            9: 30,
+            10: 31,
+            11: 30,
+            12: 31,
+        }
         days = list_days_in_a_month[month]
         if self._verification_of_high_year(year) and month == 2:
             return days + 1
-        else:
-            return days
+        return days
 
-    def _num_all_days_year(self, year) -> int: # кол. дней в году
+    def _num_all_days_year(self, year) -> int:  # кол. дней в году
         if self._verification_of_high_year(year):
             return 366
-        else:
-            return 365
+        return 365
 
-    def _num_days_from_the_new_year(self, days:int, month:int, year:int) -> int: # рассчитывает кол. дней от нового года
+    def _num_days_from_the_new_year(
+        self, days: int, month: int, year: int
+    ) -> int:  # рассчитывает кол. дней от нового года
         all_days = 0
 
         for i in range(1, month):
             all_days += self._num_days_in_a_month(i, year)
         all_days += days
- 
+
         return all_days
-    
-    def out_days_from_the_new_year(self) -> str: # для вывода кол. дней от нового года
+
+    def out_days_from_the_new_year(self) -> str:  # для вывода кол. дней от нового года
         return f"Дней с нового года = {self._num_days_from_the_new_year(days= self.days, month= self.month, year= self.year)}"
 
     def days_passed_zero_point(self) -> int:
@@ -68,7 +80,7 @@ class Date:
             all_days += self._num_all_days_year(i)
 
         return all_days
-    
+
     def add_days(self, days: int):
         sum_days = self.days + days
         if sum_days > self._num_days_in_a_month(self.month, self.year):
@@ -88,7 +100,7 @@ class Date:
             while sum_month > 12:
                 self.year += 1
                 sum_month -= 12
-            
+
             self.month = sum_month
         else:
             self.month = sum_month
